cbe96b8b455f6aee2c6a457ce8687d554c02b2d2
jithsmoth <jithsmoth.dev@gmail.com>
2022-11-29 07:18:25 +0700

    fix: config

--- vite.config.ts
+++ vite.config.ts
@@ -12,5 +12,6 @@ export default defineConfig({
     // Do not inline images and assets to avoid the phaser error
     // "Local data URIs are not supported"
     assetsInlineLimit: 0
-  }
+  },
+  base: ''
 })
